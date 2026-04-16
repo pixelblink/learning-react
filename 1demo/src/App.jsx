b29408f74db5913bc467ddf3668cb75efbe7ee32
pixelblink <chandandhare98@gmail.com>
2026-04-17 01:43:19 +0530

    day4

---
+++ 1demo/src/App.jsx
@@ -0,0 +1,22 @@
+import Page from './page1.jsx'
+
+function App(){
+
+  let name="YesSirrrrrr";
+
+  return(
+    <div>
+      <h1>this is app page</h1>
+      <h1>this is app page</h1>
+      <h1>this is app page</h1>
+      <h1>this is app page</h1>
+      <h1>this is app page</h1>
+      <h1>this is app page</h1>
+      <h1>this is app page</h1>
+      <h1>this is app page</h1>
+      {name}
+      <Page />
+    </div>
+  )
+}
+export default App
---
+++ 1demo/src/page1.jsx
@@ -0,0 +1,8 @@
+function Page(){
+    return(
+        <>
+            <h1>this is page 1</h1>
+        </>
+    )
+}
+export default Page
